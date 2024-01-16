53f02d7d9a18f7a8dcfc8410f2c359e355c334a9
lkh <lkh8033@gmail.com>
2024-01-16 20:47:41 +0900

    t

--- main.c
+++ main.c
@@ -2,46 +2,131 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-_Bool* visite; struct vertex** node;
+_Bool* visite;
+struct vertex** node;
+struct que* que;
 
 struct vertex {
 	int node;
-	struct vertex *next;
+	struct vertex* next;
 };
 
-void add(struct vertex *v, int node) {
-	if (v == NULL) {// 첫번쨰 일경우
-		v = (struct vertex*)malloc(sizeof(struct vertex));
-		v->node = node;
-		v->next = NULL;
+struct que {
+	int value;
+	struct que* next;
+};
+
+void que_add(int node) {
+	struct que* new = (struct que*)malloc(sizeof(struct que));
+	new->value = node; new->next = NULL;
+
+	if (que == NULL) {
+		que = new;
 	}
-	while (v->next != NULL) { v = v->next; }
-	v->next = (struct vertex*)malloc(sizeof(struct vertex));
-	v->next->node = node;
-	v->next->next = NULL;
+	else {
+		struct que* q = que;
+		while (q->next != NULL) { q = q->next; }
+		q->next = new;
+	}
 }
 
-void bfs(int start_node) {
+int que_pop() {
+	struct que* q = que;
+	int re;
+	if (q->next == NULL) {
+		re = q->value;
+		free(q);
+		que = NULL;
+	}
+	else {
+		re = q->value;
+		que = que->next;
+		free(q);
+	}
+	return re;
+}
+
+void add(int start, int end) {
+	struct vertex* new = (struct vertex*)malloc(sizeof(struct vertex));
+	new->node = end; new->next = NULL;
+
+	if (node[start] == NULL) {// 첫번쨰 일경우
+		node[start] = new;
+	}
+	else if (node[start]->node > end) { //첫번째 값보다 더 작은 경우
+		new->next = node[start];
+		node[start] = new;
+	}
+	else {
+		struct vertex* old = node[start];
+		struct vertex* v = node[start];
+		while (v->node < end) {
+			if (v->next == NULL) {
+				v->next = new;
+				return;
+			}
+			old = v;
+			v = v->next;
+		}
+
+		new->next = old->next;
+		old->next = new;
+	}
+}
+
+
+void dfs(int start_node) {
 	visite[start_node] = 1;
 	printf("%d ", start_node);
-	for (struct vertex* i = node[start_node]; i->node != 0; i = i->next) {
+	for (struct vertex* i = node[start_node]; i != NULL; i = i->next) {
 		if (visite[i->node] == 0) {
-			bfs(i->node);
+			dfs(i->node);
 		}
-		// 전부 돌았다면 break;
+		// 다돌았으면 break
 	}
 }
-
+void bfs() {
+	printf("%d ", que->value);
+	visite[que->value] = 1;
+	int buff;
+	while (que) {
+		//printf("%d\n", que->value);
+		for (struct vertex* i = node[buff = que_pop()]; i != NULL; i = i->next) {
+			if (visite[i->node] == 0) {
+				que_add(i->node);
+				visite[i->node] = 1;
+				printf("%d ", i->node);
+			}
+		}
+	}
+}
 int main() {
+
 	int buff1, buff2;
 	int node_n, vertex_n, start_node;
 	scanf("%d %d %d", &node_n, &vertex_n, &start_node);
-	visite = (int*)calloc(node_n,sizeof(int));
+	visite = (int*)calloc(node_n + 1, sizeof(int));
 	// _Bool visite[node_n];
-	node = (struct vertex**)calloc(node_n, sizeof(struct vertex *));
+	node = (struct vertex**)calloc(node_n + 1, sizeof(struct vertex*));
 	for (int i = vertex_n; i != 0; i--) {
 		scanf("%d %d", &buff1, &buff2);
-		add(node[buff1], buff2);
+		add(buff1, buff2);
+		add(buff2, buff1); // 양방향 연결노드
 	}
-	bfs(start_node);
+
+	/*for (int i = 1; i <= node_n; i++) {
+		for(struct vertex *v = node[i]; v!=NULL; v=v->next){
+			printf("%d -> %d \n", i, v->node);
+		}
+	}*/
+
+	dfs(start_node);
+	printf("\n");
+	que = (struct que*)malloc(sizeof(struct que));
+	que->value = start_node;
+	que->next = NULL;
+	for (int i = 1; i <= node_n; i++) {
+		visite[i] = 0;
+	}
+	bfs();
 }
